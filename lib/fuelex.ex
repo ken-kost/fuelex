b351e5278fa38c25f81de073f7383eb0065e2bf6
ken-kost <kenneth.kostresevic@gmail.com>
2025-02-12 11:25:08 +0100

    Implement calculate_recursive
    Calculates the necessary fuel for a given mass, gravity and formula params

--- lib/fuelex.ex
+++ lib/fuelex.ex
@@ -9,6 +9,16 @@ defmodule Fuelex do
     "mars" => 3.711
   }
 
+  defp calculate_recursive(mass, gravity, constant, residual, fuel_acc) do
+    case floor(mass * gravity * constant - residual) do
+      fuel when fuel <= 0 ->
+        fuel_acc
+
+      fuel ->
+        calculate_recursive(fuel, gravity, constant, residual, fuel_acc + fuel)
+    end
+  end
+
   defp validate_path(path) do
     planets = Map.keys(@gravities)
 
