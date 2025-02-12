7ecddc22aa5c32746485daef542464ba8f9c58d7
ken-kost <kenneth.kostresevic@gmail.com>
2025-02-12 13:11:26 +0100

    Add ash_ai
    also add related deps that are missing
    also add igniter

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule Fuelex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ash_ai, github: "ash-project/ash_ai"},
+      {:ash_json_api, "~> 1.4", override: true},
+      {:igniter, "~> 0.5.23"}
     ]
   end
 end
