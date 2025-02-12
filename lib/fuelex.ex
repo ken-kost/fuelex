b2ac3f7c6a7c11b9f1b88b6a6995644e2e0dd5d4
ken-kost <kenneth.kostresevic@gmail.com>
2025-02-12 11:10:19 +0100

    Init

---
+++ lib/fuelex.ex
@@ -0,0 +1,18 @@
+defmodule Fuelex do
+  @moduledoc """
+  Documentation for `Fuelex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fuelex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fuelex_test.exs
@@ -0,0 +1,8 @@
+defmodule FuelexTest do
+  use ExUnit.Case
+  doctest Fuelex
+
+  test "greets the world" do
+    assert Fuelex.hello() == :world
+  end
+end
